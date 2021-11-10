4d2bf2e1b20c0684052b6837932ce4fab61fa05a
Tom Cohen <Tomco@outlook.co.il>
2021-11-10 14:07:21 +0200

    Timer, Background, Vibrate, Glide, Toast, Models.

---
+++ app/src/main/java/com/example/triviaapp/Models/Question.java
@@ -0,0 +1,30 @@
+package com.example.triviaapp.Models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Question {
+    private String image = "";
+    private List<String> answers = new ArrayList<>();
+
+    public Question() {
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public Question setImage(String image) {
+        this.image = image;
+        return this;
+    }
+
+    public List<String> getAnswers() {
+        return answers;
+    }
+
+    public Question setAnswers(List<String> answers) {
+        this.answers = answers;
+        return this;
+    }
+}
